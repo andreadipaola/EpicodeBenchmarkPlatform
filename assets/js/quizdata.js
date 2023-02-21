e5482619f5ec674b233ffa814fc0bd764069c12e
domenico2003 <domdat03@gmail.com>
2023-02-21 15:25:56 +0100

    cominciato stampare domande

--- assets/js/quizdata.js
+++ assets/js/quizdata.js
@@ -1,4 +1,159 @@
-const quizData = {
-    domanda: ' Come funziona java?',
-    a: ''
+const questions = [
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "What does CPU stand for?",
+      correct_answer: "Central Processing Unit",
+      incorrect_answers: [
+        "Central Process Unit",
+        "Computer Personal Unit",
+        "Central Processor Unit",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+      correct_answer: "Final",
+      incorrect_answers: ["Static", "Private", "Public"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "The logo for Snapchat is a Bell.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question:
+        "Pointers were not used in the original C programming language; they were added later on in C++.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the most preferred image format used for logos in the Wikimedia database?",
+      correct_answer: ".svg",
+      incorrect_answers: [".png", ".jpeg", ".gif"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "In web design, what does CSS stand for?",
+      correct_answer: "Cascading Style Sheet",
+      incorrect_answers: [
+        "Counter Strike: Source",
+        "Corrective Style Sheet",
+        "Computer Style Sheet",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "What is the code name for the mobile operating system Android 7.0?",
+      correct_answer: "Nougat",
+      incorrect_answers: [
+        "Ice Cream Sandwich",
+        "Jelly Bean",
+        "Marshmallow",
+      ],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question: "On Twitter, what is the character limit for a Tweet?",
+      correct_answer: "140",
+      incorrect_answers: ["120", "160", "100"],
+    },
+    {
+      category: "Science: Computers",
+      type: "boolean",
+      difficulty: "easy",
+      question: "Linux was first created as an alternative to Windows XP.",
+      correct_answer: "False",
+      incorrect_answers: ["True"],
+    },
+    {
+      category: "Science: Computers",
+      type: "multiple",
+      difficulty: "easy",
+      question:
+        "Which programming language shares its name with an island in Indonesia?",
+      correct_answer: "Java",
+      incorrect_answers: ["Python", "C", "Jakarta"],
+    },
+  ];
+
+
+let risposte = document.getElementsByClassName('.risposte')
+let next = document.getElementById('prossimo')
+let target = document.querySelector('.answer-container')
+//questions[i].incorrect_answers
+
+let domandeRisposte = []
+
+for (let opzioni  of questions) {
+ let option=[]
+ let titoli= opzioni.question
+ 
+ 
+  let sbagliate = opzioni.incorrect_answers
+  sbagliate.push(opzioni.correct_answer)
+  option.push(sbagliate)
+
+
+
+  domandeRisposte.push({
+    domanda : titoli ,
+    risposte : option
+  })
+}
+
+
+if(domandeRisposte !== undefined){
+next.addEventListener('click',()=>{
+  let targeTitolo = document.querySelector('.title-box')
+  target.textContent=''
+  targeTitolo.textContent=''
+
+  let bottone = document.createElement('button')
+  bottone.classList.add('risposte')
+  let indice= Math.floor(Math.random()*domandeRisposte.length)
+
+  for (let risposta of domandeRisposte[indice].risposte) {
+    
+    for (let singola of risposta) {
+      
+      bottone.textContent = singola 
+      target.append(bottone)
+      
+    }
+    
+
+  }
+
+  domandeRisposte.splice(indice,1)
+
+  
+  let titolo = document.createElement('p')
+  titolo.classList.add('titolo')
+  titolo.textContent = domandeRisposte[indice].domanda
+  targeTitolo.appendChild(titolo)
+})
+}else{
+  next.addEventListener('click')
 }
