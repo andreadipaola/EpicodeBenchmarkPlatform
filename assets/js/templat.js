cac6abe05353917b08401f7fe6f144432b1c7641
domenico2003 <domdat03@gmail.com>
2023-02-24 11:13:08 +0100

    aggiornamento riesultpage

--- assets/js/templat.js
+++ assets/js/templat.js
@@ -307,6 +307,65 @@ proceed.addEventListener('click', () => {
 
                                 //  inserire javascript dei risultati
 
+                                                                let correctAnswers = 8;
+                                let totalQuestions = 10;
+                                let percentage = Math.round(correctAnswers / totalQuestions * 100);
+
+                                let ctx = document.getElementById('myChart').getContext('2d');
+                                let myChart = new Chart(ctx, {
+                                    type: 'doughnut',
+                                    data: {
+                                        labels: ['Correct Answers', 'Incorrect Answers'],
+                                        datasets: [{
+                                            label: '% Correct',
+                                            data: [percentage, 100 - percentage],
+                                            backgroundColor: ['#00ffff', '#d20094']
+                                        }]
+                                    },
+                                    options: {
+                                        responsive: true,
+                                        maintainAspectRatio: false,
+                                        legend: {
+                                            position: 'bottom'
+                                        },
+                                        title: {
+                                            display: true,
+                                            text: 'Quiz Results'
+                                        },
+                                        animation: {
+                                            animateScale: true,
+                                            animateRotate: true
+                                        },
+                                        tooltips: {
+                                            enabled: false
+                                        },
+                                        plugins: {
+                                            datalabels: {
+                                                formatter: function(value, context) {
+                                                    return context.chart.data.labels[context.dataIndex] + '\n' + value + '%';
+                                                },
+                                                color: '#fff',
+                                                font: {
+                                                    weight: 'bold',
+                                                    size: 16
+                                                }
+                                            }
+                                        }
+                                    }
+                                });
+
+                                let message = '';
+                                if (percentage >= 70) {
+                                message = 'Congratulations! You passed the exam. I will send you the certificate in a few minutes. Please check your email (including promotions and spam folder).';
+                                } else {
+                                message = 'Oh no! Unfortunately, you did not pass the exam. Better luck next time.';
+                                }
+
+                                let container = document.querySelector('.container');
+                                let messageDiv = document.createElement('div');
+                                messageDiv.classList.add('message');
+                                messageDiv.textContent = message;
+                                container.appendChild(messageDiv);
 
                                 //###########################################################################################################
                                 //                                      feedback page
